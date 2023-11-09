6185256944347f772cc84cbc33600eaa5f79707b
ayoniyi <ayo.a.niyi@gmail.com>
2023-11-08 17:06:56 -0700

    init commit

--- src/api/jupita-course/routes/jupita-course.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * jupita-course router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::jupita-course.jupita-course');
--- src/api/jupita-course/services/jupita-course.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * jupita-course service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::jupita-course.jupita-course');
